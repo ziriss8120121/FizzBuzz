5deb1d34715a477118f495efdd5310e9e7399a02
Yujiro Yukawa <yuj.yuk@icloud.com>
2024-03-12 15:50:34 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,18 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 1;i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
 
 	}
 
+	}
 }
